2939bfc4dcf08adae54ebf266a86b5f843412dfc
Daft <daftdevs@gmail.com>
2023-01-11 23:50:20 +0700

    feat: init file for all the constant

---
+++ src/constant.ts
@@ -0,0 +1,17 @@
+export const PRICE_FILTER_OPTION = [
+  { label: "$ - Cheap", value: 1 },
+  { label: "$$ - Moderate", value: 2 },
+  { label: "$$$ - Expensive", value: 3 },
+  { label: "$$$$ - Luxury", value: 4 },
+];
+
+export const ATTRIBUTES_FILTER_OPTION = [
+  { label: "Hot And New", value: "hot_and_new" },
+  { label: "Request A Quote", value: "request_a_quote" },
+  { label: "Reservation", value: "reservation" },
+  { label: "Waitlist Reservation", value: "waitlist_reservation" },
+  { label: "Deals", value: "deals" },
+  { label: "Gender Neutral Restrooms", value: "gender_neutral_restrooms" },
+  { label: "Open To All", value: "open_to_all" },
+  { label: "Wheelchair Accessible", value: "wheelchair_accessible" },
+];

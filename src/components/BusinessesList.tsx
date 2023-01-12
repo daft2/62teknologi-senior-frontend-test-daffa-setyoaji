97c60b3d6e5e2b1da8d05004fd7aa2eab4910d0b
Daft <daftdevs@gmail.com>
2023-01-12 19:28:49 +0700

    feat: add navigate on each item

--- src/components/BusinessesList.tsx
+++ src/components/BusinessesList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Business, Categories } from "../types";
 
 type Props = {
@@ -24,32 +25,46 @@ const BusinessesList: React.FC<Props> = ({ businesses, isLoading }) => {
   );
 };
 
-const Item = ({ data }: { data: Business }) => (
-  <div className="flex flex-col bg-white rounded overflow-hidden relative shadow">
-    <img
-      src={data.image_url}
-      alt="Business Image"
-      className="object-cover h-[10rem]"
-    />
-    <span className="absolute top-[8rem] rounded-r bg-white px-2">
-      {data.price}
-    </span>
-    <div className="flex flex-col p-2 gap-4 h-full">
-      <div className="flex flex-col">
-        <span className="text-lg">{data.name}</span>
-        <span className="text-sm text-gray-500">
-          {data.location.address1}, {data.location.city}, {data.location.state}
-        </span>
-      </div>
-      <div className="flex flex-wrap whitespace-nowrap gap-2">
-        {data.categories.map((category: Categories) => (
-          <>
-            <span className="border rounded p-2 text-xs">{category.title}</span>
-          </>
-        ))}
+const Item = ({ data }: { data: Business }) => {
+  const navigate = useNavigate();
+  const handleNavigate = () =>
+    navigate(`/${data.id}`, { state: { businessId: data.id } });
+
+  return (
+    <div
+      onClick={handleNavigate}
+      className="flex flex-col bg-white rounded overflow-hidden relative shadow cursor-pointer hover:scale-105 duration-300"
+    >
+      <img
+        src={data.image_url}
+        alt="Business Image"
+        className="object-cover h-[10rem]"
+      />
+      <span className="absolute top-[8rem] rounded-r bg-white px-2">
+        {data.price}
+      </span>
+      <div className="flex flex-col p-2 gap-4 h-full">
+        <div className="flex flex-col">
+          <span className="text-lg truncate whitespace-nowrap">
+            {data.name}
+          </span>
+          <span className="text-sm text-gray-500 truncate whitespace-nowrap">
+            {data.location.address1}, {data.location.city},{" "}
+            {data.location.state}
+          </span>
+        </div>
+        <div className="flex flex-wrap whitespace-nowrap gap-2">
+          {data.categories.map((category: Categories) => (
+            <>
+              <span className="border rounded p-2 text-xs">
+                {category.title}
+              </span>
+            </>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BusinessesList;

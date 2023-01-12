9e093e5997c77031d1716b9d408394753a3104b3
Daft <daftdevs@gmail.com>
2023-01-12 15:47:44 +0700

    ref: add params filter

--- src/api/BusinessAPI.ts
+++ src/api/BusinessAPI.ts
@@ -1,12 +1,21 @@
 import axios from "../utils/Axios";
 
 const BusinessAPI = {
-  get: async ({ location, term }: { location?: string; term?: string }) => {
-    const params = {
+  get: async ({
+    location,
+    term,
+    filter,
+  }: {
+    location?: string;
+    term?: string;
+    filter: { attributes: string };
+  }) => {
+    let params = {
       sort_by: "best_match",
       limit: 18,
       location: location,
       term: term,
+      ...filter,
     };
 
     const data = await axios.get("/search", {

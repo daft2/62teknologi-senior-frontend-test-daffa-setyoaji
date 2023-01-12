bb86a3686322d2bdab41d004ce35d2a40f9734dd
Daft <daftdevs@gmail.com>
2023-01-12 19:27:58 +0700

    feat: - refactor template div style - add new route for detailed view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Business from "./domains/business/Business";
 import Homepage from "./domains/homepage/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       {/* Layout */}
-      <div className="overflow-x-hidden">
+      <div className="overflow-x-hidden h-screen bg-neutral-50 font-robotoMono font-bold tracking-tight">
         {/* Routes */}
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/:businessId" element={<Business />} />
         </Routes>
       </div>
     </BrowserRouter>

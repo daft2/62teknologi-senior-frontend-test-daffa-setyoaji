d2046cb861e0cd4e5a54ab81521f26abb0e6fb7f
Daft <daftdevs@gmail.com>
2023-01-12 19:07:20 +0700

    feat: pagination function

--- src/domains/homepage/Homepage.tsx
+++ src/domains/homepage/Homepage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BusinessAPI from "../../api/BusinessAPI";
 import AttributesFilter from "../../components/AttributesFilter";
 import BusinessesList from "../../components/BusinessesList";
+import Pagination from "../../components/Pagination";
 import SearchBar from "../../components/SearchBar";
 import useDebounce from "../../hooks/useDebounce";
 
@@ -11,9 +12,11 @@ const Homepage = (props: Props) => {
   const [businesses, setBusinesses] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(true);
-  const [filter, setFilter] = useState<{ attributes: string }>({
+  const [filter, setFilter] = useState<{ attributes: string; offset: number }>({
     attributes: "",
+    offset: 0,
   });
+  const [pages, setPages] = useState<number>(1);
 
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
@@ -24,17 +27,25 @@ const Homepage = (props: Props) => {
       term: debouncedSearchTerm,
       filter: filter,
     })
-      .then((response) => setBusinesses(response.data.businesses))
+      .then((response) => {
+        setBusinesses(response.data.businesses);
+        setPages(Math.ceil(response.data.total / 18));
+      })
       .catch((error) => console.error(error))
       .finally(() => setIsLoading(false));
   };
 
   useEffect(() => {
     getBusiness();
-  }, [debouncedSearchTerm, filter.attributes]);
+  }, [debouncedSearchTerm, filter.attributes, filter.offset]);
 
   const handleFilterAttributes = (values: string[]) =>
     setFilter((prev) => ({ ...prev, attributes: values.toString() }));
+
+  const handleChangePage = (value: number) => {
+    if (value > 1) setFilter((prev) => ({ ...prev, offset: value * 18 }));
+    else setFilter((prev) => ({ ...prev, offset: 0 }));
+  };
 
   return (
     <div className="h-screen bg-neutral-50 font-robotoMono font-bold tracking-tight">
@@ -45,6 +56,10 @@ const Homepage = (props: Props) => {
             onSelect={(values) => handleFilterAttributes(values)}
           />
           <BusinessesList isLoading={isLoading} businesses={businesses} />
+          <Pagination
+            totalPages={pages}
+            onChange={(value) => handleChangePage(value as number)}
+          />
         </div>
       </div>
     </div>

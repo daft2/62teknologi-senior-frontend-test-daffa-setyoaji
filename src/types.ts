daad898865160bf9e173d17fc012842ed7128f6e
Daft <daftdevs@gmail.com>
2023-01-12 20:11:48 +0700

    feat: add business detail type

--- src/types.ts
+++ src/types.ts
@@ -26,13 +26,44 @@ export type Coordinates = {
 
 export type Location = {
   address1: string;
+  address2: string;
+  address3: string;
   city: string;
-  zip_code: string;
-  countery: string;
+  country: string;
+  display_address: string[];
   state: string;
+  zip_code: string;
 };
 
 export type FilterOption = {
   label: string;
   value: string | number | null;
+};
+
+export type BusinessDetail = {
+  alias: string;
+  categories: Categories[];
+  coordinates: Coordinates;
+  display_phone: string;
+  distance?: number;
+  id: string;
+  image_url: string;
+  is_claimed: boolean;
+  is_closed: boolean;
+  location: Location;
+  name: string;
+  phone: string;
+  photos: string[];
+  price: string;
+  rating: number;
+  review_count: number;
+  url: string;
+  attributes?: {
+    business_temp_closed: number;
+    outdoor_seating: boolean;
+  };
+  messaging?: {
+    url: string;
+    use_case_text: string;
+  };
 };
